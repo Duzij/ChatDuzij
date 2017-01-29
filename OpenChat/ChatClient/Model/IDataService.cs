8be33d924408149ffc0e60d9f7a7def8eda154ce
Maxim Dužij <dumax97@gmail.com>
2017-01-29 20:52:52 +0100

    New Mvvm client added [epic code]

---
+++ OpenChat/ChatClient/Model/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChatClient.Model
+{
+    public interface IDataService
+    {
+        void GetData(Action<DataItem, Exception> callback);
+    }
+}
---
+++ OpenChat/ChatClient/ViewModel/LoginViewModel.cs
@@ -0,0 +1,47 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using GalaSoft.MvvmLight.Command;
+
+namespace OpenChatClient.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _username;
+
+        private string _password;
+
+        public LoginViewModel()
+        {
+            MyCommand = new RelayCommand<object>(OnLoginCommand);
+        }
+
+        public string Username
+        {
+            get { return _username; }
+            set { _username = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+        public RelayCommand<object> MyCommand
+        {
+            get;
+            private set;
+        }
+
+        public void OnLoginCommand(object commandParameter)
+        {
+            var password = ((PasswordBox)commandParameter).Password;
+            var username = Username;
+        }
+    }
+}
