19f7f9f1630c9d92a069cc9b1ec4b832ad971f9e
Maxim Dužij <dumax97@gmail.com>
2017-02-08 23:10:33 +0100

    Basic unit test project added

---
+++ OpenChat/ChatTests/ApiTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ChatDuzijCore.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenChat.Communication;
+using OpenChat.Repositories;
+
+namespace ChatTests
+{
+    [TestClass]
+    public class ApiTests
+    {
+        public RoomRepository RoomRepository { get; set; }
+        public UserRepository UserRepository { get; set; }
+        public ChatHub ChatHub { get; set; }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            ChatHub = new ChatHub();
+        }
+
+        [TestMethod]
+        public void AddingRoom()
+        {
+            RoomRepository roomRepository = new RoomRepository();
+            roomRepository.AddRoom("TestRoom");
+        }
+    }
+}
