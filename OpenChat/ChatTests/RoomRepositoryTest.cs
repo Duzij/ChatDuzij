583efcf17ce4f2384c496068bc363dcdf031ed71
Maxim Dužij <dumax97@gmail.com>
2017-02-12 13:21:47 +0100

    Tests added

--- OpenChat/ChatTests/RoomRepositoryTest.cs
+++ OpenChat/ChatTests/RoomRepositoryTest.cs
@@ -1,19 +1,29 @@
 ﻿using System;
+using ChatDuzijCore.Repositories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenChat.Repositories;
+using OpenChatClient.Models;
 
 namespace ChatTests
 {
     [TestClass]
     public class RoomRepositoryTest
     {
-        public const string TestName = "TestRoom";
         public RoomRepository RoomRepository { get; set; }
+        public UserRepository UserRepository { get; set; }
+
+        public Message msg { get; set; } = new Message() { Author = "TestUser", Room = "TestRoom", Text = "TestMessage" };
+        public User user { get; set; } = new User() { Username = "TestUser", Password = "testPassword" };
+
+        public Room room { get; set; } = new Room() { RoomName = "TestRoom" };
 
         [TestInitialize]
         public void Setup()
         {
             RoomRepository = new RoomRepository();
+            UserRepository = new UserRepository();
+            UserRepository.AddUser(user);
+            RoomRepository.AddRoom(room.RoomName);
         }
 
         [TestMethod]
@@ -26,15 +36,25 @@ namespace ChatTests
         [TestMethod]
         public void AddRoom()
         {
-            var room = RoomRepository.FindAll().Count;
-            RoomRepository.AddRoom(TestName);
-            Assert.Equals(room++, RoomRepository.FindAll().Count);
+            var roomCount = RoomRepository.FindAll().Count;
+            RoomRepository.AddRoom(room.RoomName + "2");
+            Assert.AreEqual(roomCount + 1, RoomRepository.FindAll().Count);
+            RoomRepository.DeleteRoom(room.RoomName + "2");
+        }
+
+        [TestMethod]
+        public void AddUserToRoom()
+        {
+            var usersCount = RoomRepository.Find(room.RoomName).Users.Count;
+            RoomRepository.JoinRoom(user.Username, room.RoomName);
+            Assert.AreEqual(usersCount + 1, RoomRepository.Find(room.RoomName).Users.Count);
         }
 
         [TestCleanup]
         public void RepositoryCleanup()
         {
-            RoomRepository.DeleteRoom(TestName);
+            RoomRepository.DeleteRoom(room.RoomName);
+            UserRepository.DeleteUser(user);
         }
     }
 }
